79ba83455a77464457035b973bc10ddfad021f22
Marouane Rassili <mrassili@aol.com>
2018-08-29 22:56:23 +0100

    Improve App and Pet components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      pets: []
+    };
   }
 
-  handleTitleClick() {
-    alert("You just clicked the title!");
+  componentDidMount() {
+    petfinder.pet
+      .find({
+        output: "full",
+        location: "Los Angelos, CA"
+      })
+      .then(data => {
+        let pets = [];
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          // at least one pet is found
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            // more than 1 pet : array
+            // assign that array to pets
+            pets = data.petfinder.pets.pet;
+          } else {
+            // only one pet : object
+            // wrap it in an array
+            // assign the array to pets
+            pets = [data.petfinder.pets.pet];
+          }
+        }
+
+        this.setState({
+          // ES6 object literal shorthand
+          pets
+        });
+      });
   }
 
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>AdoptMe</h1>
-        <Pet name="Gohan" animal="Dog" breed="Husky" />
-        <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-        <Pet name="Luna" animal="Cat" breed="Siamese" />
+        <h1>AdoptMe</h1>
+        <div>
+          {this.state.pets.map(pet => {
+            let breed = "";
+            // check if pet has more than one breed
+            if (Array.isArray(pet.breeds.breed)) {
+              // pet has more than one breed
+              // join them with a comma
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              // pet has only one breed
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                name={pet.name}
+                animal={pet.animal}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
