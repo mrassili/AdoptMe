8138ed07d37a81cc8f09252460d3bfd38bb4e6b9
Marouane Rassili <mrassili@aol.com>
2018-09-09 23:38:57 +0100

    Create SearchBox component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,73 @@ import { Router, Link } from "@reach/router";
 import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import pf from "petfinder-client";
+import { SearchProvider } from "./SearchContext";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: "Los Angeles, CA",
+      animal: "",
+      breed: "",
+      breeds: "",
+      handleAnimalChange: this.handleAnimalChange,
+      handleBreedChange: this.handleBreedChange,
+      handleLocationChange: this.handleLocationChange,
+      getBreeds: this.getBreeds
+    };
+  }
+  handleLocationChange = event => {
+    // when you change the location in the input
+    // a Change event is fired and this event bubbles all the way up
+    // to the root div which is listening to changes for us
+    // when that happens, React detects a change and re-renders
+    // the input value is thus reset to state.location default value
+    // hence the "inability" to type in the input
+    // solution is to two-way bind the state to the input like so :
+    this.setState({
+      location: event.target.value
+    });
+  };
+  handleAnimalChange = event => {
+    this.setState(
+      {
+        animal: event.target.value,
+        breed: ""
+      },
+      this.getBreeds
+    );
+  };
+  getBreeds() {
+    if (this.state.animal) {
+      petfinder.breed.list({ animal: this.state.animal }).then(data => {
+        if (
+          data.petfinder &&
+          data.petfinder.breeds &&
+          Array.isArray(data.petfinder.breeds.breed)
+        ) {
+          this.setState({ breeds: data.petfinder.breeds.breed });
+        } else {
+          this.setState({ breeds: [] });
+        }
+      });
+    } else {
+      // no animal selected
+      this.setState({ breeds: [] });
+    }
+  }
+  handleBreedChange = event => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
   render() {
     return (
       <div>
@@ -16,11 +81,14 @@ class App extends React.Component {
         Depending on the route i.e. / or /details/:id
         React renders one of those components
         */}
-        <Router>
-          <Results path="/" />
-          <Details path="/details/:id" />
-          <SearchParams path="/search-params" />
-        </Router>
+        <SearchProvider value={this.state}>
+          {/* Anything under SearchProvider will have access to this.state and can update it */}
+          <Router>
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
+          </Router>
+        </SearchProvider>
       </div>
     );
   }
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,108 +1,11 @@
 import React from "react";
-import pf, { ANIMALS } from "petfinder-client";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
+import SearchBox from "./SearchBox";
 
 class SearchParams extends React.Component {
-  state = {
-    location: "Los Angeles, CA",
-    animal: "",
-    breed: "",
-    breeds: []
-  };
-  handleLocationChange = event => {
-    // when you change the location in the input
-    // a Change event is fired and this event bubbles all the way up
-    // to the root div which is listening to changes for us
-    // when that happens, React detects a change and re-renders
-    // the input value is thus reset to state.location default value
-    // hence the "inability" to type in the input
-    // solution is to two-way bind the state to the input like so :
-    this.setState({
-      location: event.target.value
-    });
-  };
-  handleAnimalChange = event => {
-    this.setState(
-      {
-        animal: event.target.value,
-        breed: ""
-      },
-      this.getBreeds
-    );
-  };
-  getBreeds() {
-    if (this.state.animal) {
-      petfinder.breed.list({ animal: this.state.animal }).then(data => {
-        if (
-          data.petfinder &&
-          data.petfinder.breeds &&
-          Array.isArray(data.petfinder.breeds.breed)
-        ) {
-          this.setState({ breeds: data.petfinder.breeds.breed });
-        } else {
-          this.setState({ breeds: [] });
-        }
-      });
-    } else {
-      // no animal selected
-      this.setState({ breeds: [] });
-    }
-  }
-  handleBreedChange = event => {
-    this.setState({
-      breed: event.target.value
-    });
-  };
   render() {
     return (
-      <div className="search-params">
-        <label htmlFor="location">
-          Location
-          <input
-            onChange={this.handleLocationChange}
-            id="location"
-            value={this.state.location}
-            placeholder="Location"
-          />
-        </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={this.state.animal}
-            onChange={this.handleAnimalChange}
-            onBlur={this.handleAnimalChange}
-          >
-            <option />
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={this.state.breed}
-            onChange={this.handleBreedChange}
-            onBlur={this.handleBreedChange}
-            disabled={!this.state.breeds.length}
-          >
-            <option />
-            {this.state.breeds.map(breed => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button>Submit</button>
+      <div className="search-route">
+        <SearchBox />
       </div>
     );
   }
