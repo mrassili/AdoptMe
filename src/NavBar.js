e10ec00fe59dfc94b43c900c4253a3fc4117b48a
Marouane Rassili <mrassili@aol.com>
2018-09-16 19:28:43 +0100

    Add styling to NavBar inside JS

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,47 @@
 import React from "react";
 import { Link } from "@reach/router";
+import styled, { keyframes } from "react-emotion";
+import colors from "./colors";
+// react-emotion is a wrapper around core emotion library
+// both are required.
+
+const Spin = keyframes`
+  from {
+    transform: rotate(0deg)
+  }
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+const SpyGlass = styled("span")`
+  display: inline-block;
+  animation: 1s ${Spin} linear infinite;
+`;
+
+const Container = styled("header")`
+  background-color: ${colors.light};
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
+
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 const NavBar = () => (
-  <header>
-    <Link to="/">AdoptMe</Link>
-    <Link to="/search-params">
-      <span aria-label="search" role="img">
+  <Container>
+    <NavLink to="/">AdoptMe</NavLink>
+    <NavLink to="/search-params">
+      {/* eslint-disable-next-line jsx-a11y/accessible-emoji*/}
+      <SpyGlass aria-label="search" role="img">
         🔍
-      </span>
-    </Link>
-  </header>
+      </SpyGlass>
+    </NavLink>
+  </Container>
 );
 
 export default NavBar;
