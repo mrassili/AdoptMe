6aca5e5661f5a8ad05240743ebb45df46a707a32
Marouane Rassili <mrassili@aol.com>
2018-09-16 18:35:23 +0100

    Extract NavBar component from App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import Results from "./Results";
 import Details from "./Details";
+import NavBar from "./NavBar";
 import SearchParams from "./SearchParams";
 import pf from "petfinder-client";
 import { SearchProvider } from "./SearchContext";
@@ -74,14 +75,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">AdoptMe</Link>
-          <Link to="/search-params">
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
+        <NavBar />
         {/* 
         Depending on the route i.e. / or /details/:id
         React renders one of those components
